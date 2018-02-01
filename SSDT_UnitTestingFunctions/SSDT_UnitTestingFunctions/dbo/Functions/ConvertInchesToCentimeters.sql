147ef6075ed4d32b462b21f3d2c2e142b19ecffe
Henry Wyckoff <h.wyckoff@rosemanagement.com>
2018-02-01 15:29:15 -0700

    Imported database from local db.

---
+++ SSDT_UnitTestingFunctions/SSDT_UnitTestingFunctions/dbo/Functions/ConvertInchesToCentimeters.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Henry Wyckoff
+-- Create date: 2/1/2018
+-- Description:	Given a length in inches, convert to centimeters.
+-- =============================================
+CREATE FUNCTION ConvertInchesToCentimeters 
+(		
+	@Inches float
+)
+RETURNS TABLE 
+AS
+RETURN 
+(	
+	SELECT Centimeters=cast(@Inches*2.54 as float)
+)
